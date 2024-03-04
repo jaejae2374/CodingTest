3e9448a2259f87ab8b4325991950f963868b52eb
JaeHyun Lee <80879249+jaejae2374@users.noreply.github.com>
2024-03-04 22:22:59 +0900

    Update solution.py

--- kakao/광고삽입/solution.py
+++ kakao/광고삽입/solution.py
@@ -8,22 +8,25 @@ def solution(play_time, adv_time, logs):
     total = get_seconds(play_time)
     adv = get_seconds(adv_time)
     times = [0]*(total+1)
-    ans = 0
-    answer = 0
     for log in logs:
         start, end = map(lambda x: get_seconds(x), log.split("-"))
         times[start]+=1
         times[end]-=1
+        
+    std=0; ans=0; answer=0
     for i in range(1, total+1):
         times[i]+=times[i-1]
-    s = sum(times[:adv-1])
-    ans = s
-    for i in range(1, total-adv+1):
-        s =  s-times[i-1]+times[i+adv-1]  # 시간초과 예상구간
-        if ans<s:
-            ans=s
-            answer=i
+        if i<=adv:
+            ans+=times[i-1]
+            ans = std
+        else:
+            std =  std+times[i-1]-times[i-adv-1]
+            if ans<std:
+                answer = i-adv
+                ans = std
     h = answer//3600
     m = (answer%3600)//60
     s = (answer%3600)%60
     return "%02d:%02d:%02d"%(h,m,s)
+
+
